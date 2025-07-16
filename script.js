878efc64a2d810c8eb86256a0668819f22800b90
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-07-16 17:23:37 +0800

    initial javascript planning, added board prototype json file

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-// Drag N Drop Functions
+// Get Elements
+// TODO: Use Classes for Board, Section, Task elements
 
-// Add Task Function
+// Board Elements
+var openBoardBtn = document.getElementById("openBoardBtn");
+var saveBoardBtn = document.getElementById("saveBoardBtn");
+var configureBoardBtn = document.getElementById("configureBoardBtn");
 
-// Add Section Function
+var newBoardBtn = document.getElementById("newBoardBtn");
 
-// New 
+var boardName = document.getElementById("boardName");
+var boardBgColor1 = document.getElementById("boardBgColor1");
+var boardBgColor2 = document.getElementById("boardBgColor2");
+var addSectionBtn = document.getElementById("addSectionBtn");
+
+// Section Elements
+var configureSectionBtn = document.getElementById("configureSectionBtn");
+var addTaskBtn = document.getElementById("addTaskBtn");
+
+// Task Elements
+var configureTaskBtn = document.getElementById("configureTaskBtn");
+
+// Create Functions
+// * Add Section
+// * Add Task
+
+// Read Functions
+// * Load Json Data
+// * Parse Json Data
+// * Save Json Data
+// * Load Board Template
+
+// Update Functions
+// * Drag and Drop
+// * Configure Board
+// * Configure Section
+// * Configure Task
+
+// Delete Functions
+// * Delete Section
+// * Delete Task
